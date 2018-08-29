123599bcf987a146e84ae94f5251a8c6f8de3055
Anthony Febus <febus.anthony@gmail.com>
2018-08-29 14:58:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
-"Hello, " + specialGuest + "!"
 
 var greetSpecialGuest = "" + specialGuest + "!";
+"Hello, " + specialGuest + "!";
 
 var topic = "space";
 
